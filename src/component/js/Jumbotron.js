151aca039deefe94018cfd647fd9f92b6ac501e9
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2021-10-05 06:41:24 +0700

    Add media social

--- src/component/js/Jumbotron.js
+++ src/component/js/Jumbotron.js
@@ -18,10 +18,24 @@ const Identity = () => {
           RAINFOG MUZABA
         </h2>
         <hr />
-        <div id="quote" className="mb-5 pb-5">
+        <div id="quote" className="mb-3 pb-3">
           I'm just a normal people who likes anime and JRPG game. However, i
           want to prove that my hobbies can bring myself to be better. So, i'll
           create some creation to prove it
+        </div>
+        <div id="sosmed">
+          <a href="https://github.com/ZhafranBahij">
+            <i class="bi bi-github ps-3"></i>
+          </a>
+          <a href="https://t.me/Zhafran_Bahij">
+            <i class="bi bi-telegram ps-3"></i>
+          </a>
+          <a href="https://www.instagram.com/zhafranbahij/">
+            <i class="bi bi-instagram ps-3"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/m-zhafran-bahij-6021171b8/">
+            <i class="bi bi-linkedin ps-3"></i>
+          </a>
         </div>
       </div>
     </div>
